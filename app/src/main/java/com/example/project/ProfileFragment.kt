10fb6bf138ea38d3432207fc758abc59e9b795d4
Azamat <azambylbaev@gmail.com>
2022-11-01 20:41:19 +0500

    Visibility and onViewCreated

--- app/src/main/java/com/example/project/ProfileFragment.kt
+++ app/src/main/java/com/example/project/ProfileFragment.kt
@@ -5,17 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+/* A simple [Fragment] subclass.
+* Use the [ProfileFragment.newInstance] factory method to
+* create an instance of this fragment.
+*/
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -38,15 +39,21 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.btn_logout).setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
+        /* Use this factory method to create a new instance of
+        * this fragment using the provided parameters.
+        *
+        * @param param1 Parameter 1.
+        * @param param2 Parameter 2.
+        * @return A new instance of fragment ProfileFragment.
+        */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
